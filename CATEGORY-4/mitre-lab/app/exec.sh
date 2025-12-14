922667413db6b4ff4f7532e2cf802c0306d82f77
LunaAcosta <lunaacosta300@gmail.com>
2025-12-14 12:42:05 -0600

    CATEGORY-4

---
+++ CATEGORY-4/mitre-lab/app/exec.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Script simula ejecución remota de comandos
+# Representa T1059.004 – Unix Shell
+
+echo "[INFO] Ejecutando comando whoami"
+whoami
+
+echo "[INFO] Ejecutando comando uname -a"
+uname -a
+
+echo "[INFO] Ejecutando comando id"
+id
